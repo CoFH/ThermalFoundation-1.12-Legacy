fe6e110ff6e973c1a9b44477dbae8826394869bd
King Lemming <kinglemming@gmail.com>
2014-07-29 19:45:25 -0400

    Pre-beta cleaning.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/thermalfoundation/ThermalFoundation.java
+++ src/main/java/thermalfoundation/ThermalFoundation.java
@@ -41,7 +41,7 @@ public class ThermalFoundation extends BaseMod {
 	public static final String modName = "Thermal Foundation";
 	public static final String version = "1.7.10R1.0.0B1";
 	public static final String dependencies = "required-after:CoFHCore@[" + CoFHCore.version + ",)";
-	public static final String releaseURL = "https://github.com/CoFH/ThermalFoundation/VERSION";
+	public static final String releaseURL = "http://github.com/CoFH/ThermalFoundation/blob/master/VERSION";
 
 	@Instance(modId)
 	public static ThermalFoundation instance;
