f6da006012844bbb5e231cecfc23bc1bb47bd914
King Lemming <kinglemming@gmail.com>
2015-02-14 03:13:39 -0500

    Started in on Lexicon GUI thing
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalfoundation/gui/client/GuiLexiconStudy.java
+++ src/main/java/cofh/thermalfoundation/gui/client/GuiLexiconStudy.java
@@ -2,6 +2,8 @@ package cofh.thermalfoundation.gui.client;
 
 import cofh.core.gui.GuiBaseAdv;
 import cofh.core.gui.element.TabInfo;
+import cofh.lib.gui.GuiColor;
+import cofh.lib.gui.element.ElementListBox;
 import cofh.lib.util.helpers.StringHelper;
 import cofh.thermalfoundation.gui.container.ContainerLexiconStudy;
 
@@ -13,6 +15,8 @@ public class GuiLexiconStudy extends GuiBaseAdv {
 	static final ResourceLocation TEXTURE = new ResourceLocation("thermalfoundation:textures/gui/LexiconStudy.png");
 
 	public String myInfo = "";
+
+	private ElementListBox oreList;
 
 	public GuiLexiconStudy(InventoryPlayer inventory, ContainerLexiconStudy container) {
 
@@ -36,6 +40,13 @@ public class GuiLexiconStudy extends GuiBaseAdv {
 		super.initGui();
 
 		addTab(new TabInfo(this, myInfo).setOffsets(12, 10));
+
+		oreList = new ElementListBox(this, 22, 26, 100, 164);
+		oreList.borderColor = new GuiColor(120, 120, 120, 0).getColor();
+		oreList.backgroundColor = new GuiColor(0, 0, 0, 32).getColor();
+
+		addElement(oreList);
+
 	}
 
 	@Override
