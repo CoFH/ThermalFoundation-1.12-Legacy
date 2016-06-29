28d45919e7b0d8aeb30451fa3c39e5e45d42a516
skyboy <mr.wraith.06@gmail.com>
2016-06-29 18:45:19 -0400

    Mark beta

--- src/main/java/cofh/thermalfoundation/ThermalFoundation.java
+++ src/main/java/cofh/thermalfoundation/ThermalFoundation.java
@@ -49,7 +49,7 @@ public class ThermalFoundation extends BaseMod {
 
 	public static final String modId = "ThermalFoundation";
 	public static final String modName = "Thermal Foundation";
-	public static final String version = "1.7.10R1.2.4";
+	public static final String version = "1.7.10R1.2.5B1";
 	public static final String version_max = "1.7.10R1.3.0";
 	public static final String dependencies = CoFHCore.version_group;
 	public static final String modGuiFactory = "cofh.thermalfoundation.gui.GuiConfigTFFactory";
