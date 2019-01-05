12c18686cc1795b7df4ac63527ea493a25734fc3
King Lemming <kinglemming@gmail.com>
2019-01-05 13:55:22 -0500

    Bump version.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalfoundation/ThermalFoundation.java
+++ src/main/java/cofh/thermalfoundation/ThermalFoundation.java
@@ -33,7 +33,7 @@ public class ThermalFoundation {
 	public static final String MOD_ID = "thermalfoundation";
 	public static final String MOD_NAME = "Thermal Foundation";
 
-	public static final String VERSION = "2.6.1";
+	public static final String VERSION = "2.6.2";
 	public static final String VERSION_MAX = "2.7.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
